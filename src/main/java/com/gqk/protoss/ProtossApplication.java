6747b9e3f18fe5b463addf586032c2fa988b6be1
gquik <gequankai@163.com>
2019-04-20 13:56:53 +0800

    2019/4/20-----线上版本建立

--- src/main/java/com/gqk/protoss/ProtossApplication.java
+++ src/main/java/com/gqk/protoss/ProtossApplication.java
@@ -3,10 +3,17 @@ package com.gqk.protoss;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication//(exclude = DataSourceAutoConfiguration.class)
 @MapperScan("com.gqk.protoss.dao")
-public class ProtossApplication {
+public class ProtossApplication extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ProtossApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProtossApplication.class, args);
